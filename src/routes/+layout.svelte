11ca3353ba907636c5eb2c953fee98798296d79a
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-12-21 00:12:40 +0530

    feature: github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,18 @@
 	import DarkModeToggle from '$lib/components/elements/DarkModeToggle.svelte'
 	import '../styles/app.css'
 	import { ModeWatcher } from 'mode-watcher'
+	import { Link2 as Link } from 'radix-icons-svelte'
 </script>
 
 <ModeWatcher />
 
-<div class="mt-10 container">
+<div class="mt-7 container flex items-start justify-between">
+	<a
+		class="flex items-center text-xs gap-x-2 font-mono text-blue-500"
+		href="https://github.com/goswamianshuman/todo-svelte.git"
+	>
+		<Link class="h-4 w-4" /> view on github</a
+	>
 	<DarkModeToggle />
 </div>
 <slot />
